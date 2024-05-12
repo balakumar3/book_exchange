9f21b202606da207639875c287966ca3caeab25f
Amit Bhapkar <2023mt93179@wilp.bits-pilani.ac.in>
2024-05-12 13:32:19 +0530

    Book Search and listing

--- client/src/components/BookListingAndSearch.jsx
+++ client/src/components/BookListingAndSearch.jsx
@@ -2,12 +2,17 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import Axios from 'axios'
 import './BookListingAndSearch.css'
+import bookDetails from './DummyData_Books';
 
 const BookListingAndSearch = () => {
     const [bookName, setBookName] = useState('');
 
+    const [searchTerm, setSearchTerm] = useState('');
+
     const handlesearch = (e) => {
         e.preventDefault();
+
+
         Axios.post("book search api", { bookName })
             .then(response => {
                 console.log(response)
@@ -18,36 +23,76 @@ const BookListingAndSearch = () => {
                 console.log(err)
             })
     }
+    const [dummyData, setDummyData] = useState(bookDetails);
 
-    const colors = ['The Hunger Games', 'Harry Potter and the Order of the Phoenix', 'Pride and Prejudice', 'The Book Thief', 'The Picture of Dorian Gray', 'The Perks of Being a Wallflower', 'The Great Gatsby', 'Lord of the Flies'];
+
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 5;
+    const totalPages = Math.ceil(dummyData.length / itemsPerPage);
+
+
+    const currentItem = searchTerm != '' ? dummyData: dummyData.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage) ;
+
+    const filteredBooks = currentItem.filter(book =>
+        book.BOOK_NAME.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+    const paginationButtons = Array.from({ length: totalPages }, (_, i) => (
+        <li key={i}>
+            <button
+                className={`${currentPage === i + 1 ? 'bg-blue-500 text-white hover:bg-blue-400' : 'bg-gray-200 text-gray-700'
+                    } px-3 py-2 mx-1 rounded`}
+                onClick={() => paginate(i + 1)}
+                disabled={currentPage === i + 1}
+            >
+                {i + 1}
+            </button>
+        </li>
+    ));
 
     return (
-        <div className="relative">
-            <img src="https://americanbookco.com/wp-content/uploads/2021/02/stacked-books.jpeg" alt="Your Image" className="w-full" />
-
-            <ul className="absolute top-1 right-1/2 text-center">
+        <div className="my-background">
                 <h1 className="text-3xl font-bold text-center mb-4">Find Your Book Here</h1>
-                <div>
-                    <form className='bg-white shadow-md rounded-lg px-10 py-8 mb-8' onSubmit={handlesearch}>
-                        <h2 className='text-2xl mb-6'>Search Book</h2>
-
+                <div className="items-center">
+                    <form className='bg-white shadow-md rounded-lg  px-10 py-8 mb-8' onSubmit={handlesearch}>
+                        <h2 className='text-2xl  mb-6'>Search Book</h2>
                         <div className='mb-6'>
                             <label htmlFor='text' className='block text-gray-700 text-sm font-bold mb-2'>Book Name</label>
-                            <input type='text' autoComplete='off' placeholder='Book Name' className='shadow appearance-none border rounded w-full md:w-64 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' onChange={(e) => setBookName(e.target.value)}></input>
-                        </div>
-
-                        <div className='flex items-center justify-between'>
-                            <button type='submit' className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline' >Search</button>
+                            <input type='text' autoComplete='off' placeholder='Book Name' className='shadow appearance-none border rounded w-full md:w-64 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' onChange={(e) => setSearchTerm(e.target.value)}></input>
                         </div>
                     </form>
                 </div>
-                <div className='shadow-md rounded-lg text-center px-1  py-1 bg-white height: 100%' >
-                    <label className='block text- text-sm font-bold mb-3'>Book list</label>
-                    <div className='grid'>
-                        {colors.map((color, index) => <div key={index} className="cell">{color}</div>)}
+                <div className=" overflow-x-auto">
+                <table className="table-auto w-full">
+                    <thead>
+                        <tr className= 'bg-gray-100'>
+                            <th className="px-4 py-2">BookID</th>
+                            <th className="px-4 py-2">Book Name</th>
+                            <th className="px-4 py-2">Author Name</th>
+                            <th className="px-4 py-2">Genere</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {filteredBooks.map((data, index) => (
+                            <tr key={index} className={index % 1 === 0 ? 'bg-white' : ''}>
+                                <td className="border px-4 py-2">{data.BOOK_ID}</td>
+                                <td className="border px-4 py-2">{data.BOOK_NAME}</td>
+                                <td className="border px-4 py-2">{data.AUTHOR}</td>
+                                <td className="border px-4 py-2">{data.GENERE}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+
+                {dummyData.length > itemsPerPage && (
+                    <div className="mt-4 flex justify-center">
+                        <nav className="block">
+                            <ul className="flex pl-0 rounded list-none flex-wrap">{paginationButtons}</ul>
+                        </nav>
                     </div>
-                </div >
-            </ul >
+                )}
+            </div>
             <footer className="absolute bottom-0 w-full text-center py-4 bg-gray-200">
                 &copy; 2024 Book Exchange Platform. All rights reserved.
             </footer>
---
+++ client/src/components/DummyData_Books.js
@@ -0,0 +1,41 @@
+const bookDetails = [
+    {
+        BOOK_ID: "B001",
+        BOOK_NAME:"Harry Potter",
+        AUTHOR: "J.K. Rowling",
+        GENERE: "Fantasy"
+    },
+    {
+        BOOK_ID: "B002",
+        BOOK_NAME:"Dune",
+        AUTHOR: "Isaac Asimov",
+        GENERE: "Sci-Fi"
+    },
+    {
+        BOOK_ID: "B003",
+        BOOK_NAME:"The Adventures of Sherlock Holmes",
+        AUTHOR: "Arthur Conan Doyle",
+        GENERE: "Mystery"
+    },
+    {
+        BOOK_ID: "B004",
+        BOOK_NAME:"The Girl with the Dragon Tattoo",
+        AUTHOR: "Stieg Larsson",
+        GENERE: "Thriller"
+    },
+    {
+        BOOK_ID: "B005",
+        BOOK_NAME:"The Shining ",
+        AUTHOR: "Stephen King",
+        GENERE: "Horror"
+    },
+    {
+        BOOK_ID: "B006",
+        BOOK_NAME:"Treasure Island",
+        AUTHOR: "Robert Louis Stevenson",
+        GENERE: "Adventure"
+    }
+    
+];
+
+export default bookDetails;
